2c6d6fae947e1b2cf10ed67a1277196dcd870dae
Савенок Дмитрий <36520766+DmitriySavenok@users.noreply.github.com>
2022-03-14 18:00:25 +0300

    contacts

--- src/blocks/navigation/navigation.js
+++ src/blocks/navigation/navigation.js
@@ -1,59 +1,21 @@
 var logo = document.getElementById("logo");
-  var navigation = document.getElementById("navigation");
-  var buttonOpen = document.getElementById("navigation-button-open");
-  var buttonClose = document.getElementById("navigation-button-close");
-  var navigatioList = document.querySelector(".navigation__list");
-  var navigatioBlock = document.querySelector(".navigation__block");
-
+var navigation = document.getElementById("navigation");
 
 function navigationToggle() {
-  var navigationIsClose = navigation.classList.contains("navigation-closed");
-
-  if (navigationIsClose == true) {
-    navigation.classList.remove("navigation-closed")
-    navigation.classList.add("navigation__opened")
-    buttonOpen.classList.add("visually-hidden")
-    buttonClose.classList.remove("visually-hidden")
-    navigatioList.classList.remove("visually-hidden")
-    navigatioBlock.classList.remove("visually-hidden")
-    logo.classList.add("logo-fixed")
-  }
-
-  if (navigationIsClose == false) {
-    navigation.classList.add("navigation-closed")
-    navigation.classList.remove("navigation__opened")
-    buttonOpen.classList.remove("visually-hidden")
-    buttonClose.classList.add("visually-hidden")
-    navigatioList.classList.add("visually-hidden")
-    navigatioBlock.classList.add("visually-hidden")
-    logo.classList.remove("logo-fixed")
-  }
+    navigation.classList.toggle("navigation__opened");
+    logo.classList.toggle("logo-fixed")
 }
 
-document.querySelectorAll('a[href^="#"').forEach(link => {
+$(document).ready(function() {
+  jQuery(".scrollto").click(function () {
+  elementClick = jQuery(this).attr("href")
 
-  link.addEventListener('click', function(e) {
-      e.preventDefault();
+  $('#navigation').removeClass('navigation__opened')
+  $('#logo').removeClass('logo-fixed')
 
-      let href = this.getAttribute('href').substring(1);
 
-      const scrollTarget = document.getElementById(href);
-
-      const topOffset = document.querySelector('.scrollto').offsetHeight;
-      // const topOffset = 0; // если не нужен отступ сверху
-      const elementPosition = scrollTarget.getBoundingClientRect().top;
-      const offsetPosition = elementPosition - topOffset;
-
-      navigation.classList.add("navigation-closed")
-      navigation.classList.remove("navigation__opened")
-      buttonOpen.classList.remove("visually-hidden")
-      buttonClose.classList.add("visually-hidden")
-      navigatioList.classList.add("visually-hidden")
-      logo.classList.remove("logo-fixed")
-
-      window.scrollBy({
-          top: offsetPosition,
-          behavior: 'smooth'
-      });
+  destination = jQuery(elementClick).offset().top - 70;
+  jQuery("html:not(:animated),body:not(:animated)").animate({scrollTop: destination}, 1000);
+  return false;
   });
 });
